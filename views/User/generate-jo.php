7b43a72e775e5f74bd55a02c07c665da2f0b65ef
Denver Arancillo <xdba28@gmail.com>
2018-08-25 22:14:17 +0800

    JO button

--- views/User/generate-jo.php
+++ views/User/generate-jo.php
@@ -9,7 +9,10 @@
     }else{
        Redirect::To('../../index');
         die();
-    }
+	}
+	
+
+	
    
 
 ?>
@@ -64,80 +67,98 @@
 			
 			<!-- Main Content -->
             <div class="wrapper wrapper-content">
-				<div class="wrapper wrapper-content animated fadeInRight">
+			<div class="wrapper wrapper-content animated fadeInRight">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="tabs-container">
+							<ul class="nav nav-tabs">
+								<li><a class="nav-link active" data-toggle="tab" href="#tab-3">Project &nbsp&nbsp<i class="ti-folder" style="font-size:18px"></i></a></li>
+								<li><a class="nav-link" data-toggle="tab" href="#tab-4">Particulars &nbsp&nbsp<i class="ti-pencil-alt" style="font-size:18px"></i></a></li>
+								<li><a class="nav-link" data-toggle="tab" href="#tab-5">Something &nbsp&nbsp<i class="ti-bookmark" style="font-size:18px"></i></a></li>
+							</ul>
+							<div class="tab-content">
+								<form id="agik" action="" method="POST"></form>
+								<div id="tab-3" class="tab-pane active">
+									<div class="panel-body">
+									   <h2>Project Information</h2>
 
-					<div class="row">
-						<div class="col-lg-12">
-							<div class="ibox">
-								<div class="ibox-title">
-									<h5>Wizard with Validation</h5>
-								</div>
-								<div class="ibox-content">
-									<h2>
-										Job Order Specification Requirements
-									</h2>
-									<p>
-										Specify the required fields for your need to generate the Job Order Form that suits your need.
-									</p>
-
-									<form id="form" action="#" class="wizard-big">
-										<h1>Project</h1>
-										<fieldset>
-											<h2>Project Information</h2>
-											<div class="row">
-												<div class="col-lg-8">
-												<br><br>
-													<div class="form-group">
-														<label>Project title *</label>
-														<input id="title" name="title" type="text" class="form-control">
-													</div>
-													<div class="form-group">
-														<label>Overall Cost *</label>
-														<input id="estimated_cost" name="estimated_cost" type="text" class="form-control">
-													</div>
-													<div class="form-group">
-														<label>Number of Lots</label>
-														<input id="lot" name="lot" type="number" min="1" class="form-control">
-													</div>
-
+										<p>Specify the required fields to generate the Job Order Form that suits your need.</p>
+										<div class="row">
+											<div class="col-lg-7">
+												<div class="form-group">
+													<label>Project title *</label>
+													<input form="agik" id="title" name="title" type="text" class="form-control">
 												</div>
-												<div class="col-lg-4">
-													<div class="text-center">
-														<div style="margin-top: 20px">
-															<i class="ti-layout-tab" style="font-size: 180px;color: #FFD700 "></i>
-														</div>
-													</div>
+												<div class="form-group">
+													<label>Overall Estimated Cost *</label>
+													<input id="estimated_cost" name="estimated_cost" type="text" class="form-control">
+												</div>
+												<div class="form-group">
+													<label>Number of Lots *</label>
+													<input id="lots" name="lots" type="number" min=1 class="form-control">
 												</div>
 											</div>
+											<div class="col-lg-3">
+												<div class="text-center">
+													<div style="margin-left: 100px">
+														<i class="ti-layout-tab" style="font-size: 180px;color: #FFD700 "></i>
+													</div>
+												</div>
+											</div>	
+										</div>
 
-										</fieldset>
-										<h1>Particulars</h1>
-										<fieldset>
-											<h2>Project Information</h2>
-											<div class="row" id="wf-stp-2" style="overflow-y: auto; height: 450px">
-												<!-- step 2 -->
-											</div>
-										</fieldset>
-
-										<h1>Warning</h1>
-										<fieldset>
-											<div class="text-center" style="margin-top: 120px">
-												<h2>You did it Man :-)</h2>
-											</div>
-										</fieldset>
-
-										<h1>Finish</h1>
-										<fieldset>
-											<h2>Terms and Conditions</h2>
-											<input id="acceptTerms" name="acceptTerms" type="checkbox" class="required"> <label for="acceptTerms">I agree with the Terms and Conditions.</label>
-										</fieldset>
-									</form>
+									</div>	
 								</div>
-							</div>
-							</div>
+								<div id="tab-4" class="tab-pane">
+									<div class="panel-body">
+										<h2>Particulars Setting</h2>
 
+										<p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
+											and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+
+										<div class="row">
+											<div class="col-lg-7">
+												<div class="form-group">
+													<label>fields</label>
+													<input id="title" name="title" type="text" class="form-control">
+												</div>
+												<div class="form-group">
+													<label>fields</label>
+													<input id="estimated_cost" name="estimated_cost" type="text" class="form-control">
+												</div>
+												<div class="form-group">
+													<label>Number of Lots</label>
+													<input id="lot" name="lot" type="number" min="1" class="form-control">
+												</div>
+											</div>
+											<div class="col-lg-3">
+												<div class="text-center">
+													<div style="margin-left: 100px">
+														<i class="ti-pencil-alt" style="font-size: 180px;color: #FFD700 "></i>
+													</div>
+												</div>
+											</div>	
+										</div>
+									</div>
+								</div>
+								<div id="tab-5" class="tab-pane">
+									<div class="panel-body">
+										<strong>Donec quam felis</strong>
+
+										<p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
+											and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+
+										<p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
+											sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
+									</div>
+								</div>
+								<input form="agik" type="submit">
+							</div>
 						</div>
-				</div>
+					</div>
+				
+				</div><br><br><br> <br><br><br><br><br><br><br><br><br><br><br><br><br>
+			</div>
 			
             </div>
 			<!-- Main Content End -->
@@ -154,57 +175,58 @@
     <script>
         $(document).ready(function(){
 
-			document.getElementById('lot').addEventListener('change', function()
+			$('#lot').on('change', function()
 			{
 				var lots = this.value;
+				var obj = []
 				$('#wf-stp-2').html('');
-				for(let i = 1 ; i <= lots ; i++)
+				for(let i = 0 ; i < lots ; i++)
 				{
+					obj.push({tag: 0});
 					var tmp_lot = `
 					<div class="col-lg-6">
 						<div class="ibox">
 							<div class="ibox-title">
-								<h5>Lot Number ${i}</h5>
+								<h5>Lot Number ${i + 1}</h5>
 							</div>
-							<div class="ibox-content" id="lot.${i}">
+							<div class="ibox-content" id="lot-${i}">
 								<div>
 									<p class="font-bold">Header Name: </p>
 									<input type="text" name="lot${i}[list-name][]" class="form-control">
 									<br>
 									<p class="font-bold">Tags:</p>
-									<input class="form-control" name="lot${i}[list][]" id="lot.${i}-tag-${i}" data-role="tagsinput">
+									<input class="form-control" name="lot${i}[list][]" id="lot-${i}-tag-0" data-role="tagsinput">
 									<br>
 								</div>
 							</div>
-							<button type="button" onclick="addList('lot.${i}')">Click</button>
+							<button type="button" data-type="btn" data-tag="lot-${i}">Click</button>
 							
 						</div>
 					</div>`;
 					$(`#wf-stp-2`).append(tmp_lot);
-					$(`[data-role="tagsinput"]:last`).tagsinput();
+					$(`#lot-${i}-tag-0`).tagsinput();
 				}
 
-			})
+				$('[data-type="btn"]').on('click', function()
+				{
+					var num = $(this).attr("data-tag").split("-");
+					obj[num[1]].tag++;
+					var tg_num = obj[num[1]].tag;
+					var list_tmp = `
+					<div>
+						<p class="font-bold">Header Name: </p>
+						<input type="text" name="lot${num[1]}[list-name][]" class="form-control">
+						<br>
+						<p class="font-bold">Tags:</p>
+						<input class="form-control" name="lot${num[1]}[list][]" id="lot-${num[1]}-tag-${tg_num}" data-role="tagsinput">
+						<br>
+					<div>`;
+					$(`#${num[0]}-${num[1]}`).append(list_tmp);
+					$(`#lot-${num[1]}-tag-${tg_num}`).tagsinput();
+				});
+			});
+		});
 
-	   });
-	   
-
-		function addList(lot)
-		{
-			var num = lot.split(".");
-			var list_tmp = `
-			<div>
-				<p class="font-bold">Header Name: </p>
-				<input type="text" name="lot${num[1]}[list-name][]" class="form-control">
-				<br>
-				<p class="font-bold">Tags:</p>
-				<input class="form-control" name="lot${num[1]}[list][]" data-role="tagsinput">
-				<br>
-			<div>`;
-			$(`#${lot}`).append(list_tmp);
-			// document.getElementById(lot).innerHTML += list_tmp;
-			// $('[data-role="tagsinput"]:last').tagsinput();
-		}
     </script>
 
 
--- views/User/generate-pr.php
+++ views/User/generate-pr.php
@@ -10,8 +10,10 @@
        Redirect::To('../../index');
         die();
     }
-   
+ 
 
+
+					
 ?>
 
 
@@ -69,55 +71,161 @@
             <div class="wrapper wrapper-content">
 					<!-- Content here-->
 
-				<div class="wrapper wrapper-content animated fadeInRight">
-					<div class="row">
-                <div class="col-lg-12">
-                    <div class="tabs-container">
-                        <ul class="nav nav-tabs">
-                            <li><a class="nav-link active" data-toggle="tab" href="#tab-3">Project &nbsp&nbsp<i class="ti-folder" style="font-size:18px"></i></a></li>
-                            <li><a class="nav-link" data-toggle="tab" href="#tab-4">Particulars &nbsp&nbsp<i class="ti-ruler-pencil" style="font-size:18px"></i></a></li>
-                            <li><a class="nav-link" data-toggle="tab" href="#tab-5">Something &nbsp&nbsp<i class="ti-bookmark" style="font-size:18px"></i></a></li>
-                        </ul>
-                        <div class="tab-content">
-                            <div id="tab-3" class="tab-pane active">
-                                <div class="panel-body">
-                                   <h2>Project Information</h2>
+			<div class="wrapper wrapper-content animated fadeInRight">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="tabs-container">
+							<ul class="nav nav-tabs">
+								<li><a class="nav-link active" data-toggle="tab" href="#tab-3">Project &nbsp&nbsp<i class="ti-folder" style="font-size:18px"></i></a></li>
+								<li><a class="nav-link" data-toggle="tab" href="#tab-4">Particulars &nbsp&nbsp<i class="ti-pencil-alt" style="font-size:18px"></i></a></li>
+								<li><a class="nav-link" data-toggle="tab" href="#tab-5">Something &nbsp&nbsp<i class="ti-bookmark" style="font-size:18px"></i></a></li>
+							</ul>
+							<div class="tab-content">
+								<form id="agik" action="" method="POST"></form>
+								<div id="tab-3" class="tab-pane active">
+									<div class="panel-body">
+									   <h2>Project Information</h2>
 
-                                    <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of
-                                        existence in this spot, which was created for the bliss of souls like mine.</p>
+										<p>Specify the required fields to generate the Job Order Form that suits your need.</p>
+										<div class="row">
+											<div class="col-lg-7">
+												<div class="form-group">
+													<label>Project title *</label>
+													<input id="title" name="title" type="text" class="form-control">
+												</div>
+												<div class="form-group">
+													<label>Overall Estimated Cost *</label>
+													<input id="estimated_cost" name="estimated_cost" type="text" class="form-control">
+												</div>
+												<div class="form-group">
+													<label class="font-normal"></label>
+													<div>
+														<select data-placeholder="Choose Category" class="chosen-select" multiple style="width:350px;" tabindex="4" name="category">															
+															<option value="1">Common Office Supplies</option>
+															<option value="2">Paper Materials & Products</option>          
+															<option value="3">Hardware Supplies</option>
+															<option value="4">Sporting Supplies</option>
+															<option value="5">Common Janitorial/Cleaning Supplies</option>
+															<option value="6">IT Supplies</option>
+															<option value="7">Laboratory Supplies</option>
+															<option value="8">Computer Supplies</option>
+														</select>
+													</div>
+												</div>
+											</div>
+											<div class="col-lg-3">
+												<div class="text-center">
+													<div style="margin-left: 100px">
+														<i class="ti-layout-tab" style="font-size: 180px;color: #FFD700 "></i>
+													</div>
+												</div>
+											</div>	
+										</div>
 
-                                    <p>I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at
-                                        the present moment; and yet I feel that I never was a greater artist than now. When.</p>
-                                </div>
-                            </div>
-                            <div id="tab-4" class="tab-pane">
-                                <div class="panel-body">
-                                    <strong>Donec quam felis</strong>
+									</div>	
+								</div>
+								<div id="tab-4" class="tab-pane">
+									<div class="panel-body">
+										<h2>Particulars Setting</h2>
 
-                                    <p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
-                                        and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+										<p>Some shitty explaination what the hell is going on</p>
 
-                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
-                                        sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
-                                </div>
-                            </div>
-                            <div id="tab-5" class="tab-pane">
-                                <div class="panel-body">
-                                    <strong>Donec quam felis</strong>
+										<div class="row">
+											<div class="col-lg-12">	 
+												<div class="alert alert-success">
+													Below is the Item List Form For Lot 1 - Sample Lot Category
+													
+												</div>
+												<button class="btn btn-danger pull-righ">add another list</button><br>
+												<table class="table table-bordered">
+													<thead>
+													<tr>
+														<th>#</th>
+														<th>First Name</th>
+														<th>Last Name</th>
+														<th>Username</th>
+													</tr>
+													</thead>
+													<tbody>
+													<tr>
+														<td>1</td>
+														<td>Mark</td>
+														<td>Otto</td>
+														<td>@mdo</td>
+													</tr>
+													<tr>
+														<td>2</td>
+														<td>Jacob</td>
+														<td>Thornton</td>
+														<td>@fat</td>
+													</tr>
+													<tr>
+														<td>3</td>
+														<td>Larry</td>
+														<td>the Bird</td>
+														<td>@twitter</td>
+													</tr>
+													</tbody>
+												</table>
+											</div>		
+											<div class="col-lg-12">	 
+												<div class="alert alert-success">
+													Below is the Item List Form For Lot 2 - Sample Lot Category
+													
+												</div>
+											
+												<table class="table table-bordered">
+													<thead>
+													<tr>
+														<th>#</th>
+														<th>First Name</th>
+														<th>Last Name</th>
+														<th>Username</th>
+													</tr>
+													</thead>
+													<tbody>
+													<tr>
+														<td>1</td>
+														<td>Mark</td>
+														<td>Otto</td>
+														<td>@mdo</td>
+													</tr>
+													<tr>
+														<td>2</td>
+														<td>Jacob</td>
+														<td>Thornton</td>
+														<td>@fat</td>
+													</tr>
+													<tr>
+														<td>3</td>
+														<td>Larry</td>
+														<td>the Bird</td>
+														<td>@twitter</td>
+													</tr>
+													</tbody>
+												</table>
+											</div>												
+										</div>
+									</div>
+								</div>
+								<div id="tab-5" class="tab-pane">
+									<div class="panel-body">
+										<strong>Donec quam felis</strong>
 
-                                    <p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
-                                        and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+										<p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
+											and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
 
-                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
-                                        sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-					
+										<p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
+											sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
+									</div>
+								</div>
+								
+							</div>
+						</div>
 					</div>
-				</div>
+				
+				</div><br><br><br> <br><br><br><br><br><br><br><br><br><br><br><br><br>
+			</div>
 				
 											
 				
