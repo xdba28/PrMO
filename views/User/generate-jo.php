8c781436c2668d582ec736161a74f471bbab239d
xdba28 <xdba28@gmail.com>
2018-08-29 13:58:31 +0800

    Add files via upload

--- views/User/generate-jo.php
+++ views/User/generate-jo.php
@@ -13,8 +13,6 @@
    
 
 ?>
-
-
 <!DOCTYPE html>
 <html>
 
@@ -66,80 +64,104 @@
 			
 			<!-- Main Content -->
             <div class="wrapper wrapper-content">
-				<div class="wrapper wrapper-content animated fadeInRight">
+			<div class="wrapper wrapper-content animated fadeInRight">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="tabs-container">
+							<ul class="nav nav-tabs">
+								<li><a class="nav-link active" data-toggle="tab" href="#tab-1">Project &nbsp&nbsp<i class="ti-folder" style="font-size:18px"></i></a></li>
+								<li><a class="nav-link" data-toggle="tab" href="#tab-2">Particulars &nbsp&nbsp<i class="ti-pencil-alt" style="font-size:18px"></i></a></li>
+								<li><a class="nav-link" data-toggle="tab" href="#tab-3">Signatories &nbsp&nbsp<i class="ti-user" style="font-size:18px"></i></a></li>
+							</ul>
+							<div class="tab-content">
+								
+								<div id="tab-1" class="tab-pane active">
+									<div class="panel-body">
+									   <h2>Project Information</h2>
 
-					<div class="row">
-						<div class="col-lg-12">
-							<div class="ibox">
-								<div class="ibox-title">
-									<h5>Wizard with Validation</h5>
-								</div>
-								<div class="ibox-content">
-									<h2>
-										Job Order Specification Requirements
-									</h2>
-									<p>
-										Specify the required fields for your need to generate the Job Order Form that suits your need.
-									</p>
-
-									<form id="form" action="#" class="wizard-big">
-										<h1>Project</h1>
-										<fieldset>
-											<h2>Project Information</h2>
-											<div class="row">
-												<div class="col-lg-8">
-												<br><br>
-													<div class="form-group">
-														<label>Project title *</label>
-														<input id="title" name="title" type="text" class="form-control">
-													</div>
-													<div class="form-group">
-														<label>Overall Cost *</label>
-														<input id="estimated_cost" name="estimated_cost" type="text" class="form-control">
-													</div>
-													<div class="form-group">
-														<label>Number of Lots</label>
-														<input id="lot" name="lot" type="number" min="1" class="form-control">
-													</div>
-
+										<p>Specify the required fields to generate the Job Order Form that suits your need.</p>
+										<div class="row">
+											<div class="col-lg-7">
+												<div class="form-group">
+													<label>Project title *</label>
+													<input id="title" name="title" type="text" class="form-control">
 												</div>
-												<div class="col-lg-4">
+												<div class="form-group">
+													<label>Overall Estimated Cost *</label>
+													<input id="estimated_cost" name="estimated_cost" type="text" class="form-control">
+												</div>
+												<div class="form-group">
+													<label>Number of Lots *</label>
+													<input id="lot" name="lot" type="number" min=1 class="form-control">
+												</div>
+											</div>
+											<div class="col-lg-3">
+												<div class="text-center">
+													<div style="margin-left: 100px">
+														<i class="ti-layout-tab" style="font-size: 180px;color: #FFD700 "></i>
+													</div>
+												</div>
+											</div>	
+										</div>
+
+									</div>	
+								</div>
+								<div id="tab-2" class="tab-pane">
+									<div class="panel-body">
+										<h2>Particulars Setting</h2>
+										<p>Some shitty explaination what the hell is going on</p>
+										<div class="row" id="wf-stp-2" style="overflow-y: auto; height: 450px">
+											<div class="col-lg-12">
+												<h1>No Lots Set.</h1>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div id="tab-3" class="tab-pane">
+									<div class="panel-body">
+										   <h2>Project Signatories</h2>
+
+											<p>Specify all signatories to finalized this form.</p>
+											
+											<div class="row">
+												<div class="col-lg-7">
+													<div class="form-group">
+														<label>End User *</label>
+														<input id="enduser" name="enduser" type="text" value="Nico Ativo" class="form-control" disabled>
+													</div>
+													<div class="form-group">
+														<label>Noted By *</label>
+														<input id="noted" name="noted" type="text"  class="form-control">
+													</div>
+													<div class="form-group">
+														<label>Verified By *</label>
+														<input id="verified" name="verified" type="text"  class="form-control">
+													</div>
+													<div class="form-group">
+														<label>Aproved By *</label>
+														<input id="approved" name="approved" type="text"  class="form-control">
+													</div>													
+												</div>
+												<div class="col-lg-3">
 													<div class="text-center">
-														<div style="margin-top: 20px">
-															<i class="ti-layout-tab" style="font-size: 180px;color: #FFD700 "></i>
+														<div style="margin-left: 100px;  margin-top:20px">
+															<i class="ti-user" style="font-size: 180px;color: #FFD700;"></i>
 														</div>
 													</div>
+												</div>	
+												<div class="col-md-7">
+													<button class="btn btn-primary btn-outline pull-right">Finish</button>
+													<button class="btn btn-danger btn-outline pull-right" style="margin-right:5px">Cancel</button>													
 												</div>
-											</div>
-
-										</fieldset>
-										<h1>Particulars</h1>
-										<fieldset>
-											<h2>Project Information</h2>
-											<div class="row" id="wf-stp-2" style="overflow-y: auto; height: 450px">
-												<!-- step 2 -->
-											</div>
-										</fieldset>
-
-										<h1>Warning</h1>
-										<fieldset>
-											<div class="text-center" style="margin-top: 120px">
-												<h2>You did it Man :-)</h2>
-											</div>
-										</fieldset>
-
-										<h1>Finish</h1>
-										<fieldset>
-											<h2>Terms and Conditions</h2>
-											<input id="acceptTerms" name="acceptTerms" type="checkbox" class="required"> <label for="acceptTerms">I agree with the Terms and Conditions.</label>
-										</fieldset>
-									</form>
-								</div>
+											</div>											
+									</div>
+								</div>							
 							</div>
-							</div>
-
 						</div>
-				</div>
+					</div>
+				
+				</div><br><br><br> <br><br><br><br><br><br><br><br><br><br><br><br><br>
+			</div>
 			
             </div>
 			<!-- Main Content End -->
@@ -151,158 +173,69 @@
         </div>
     </div>
 
-    <!-- Mainly scripts -->
-    <script src="../../assets/js/jquery-3.1.1.min.js"></script>
-    <script src="../../assets/js/popper.min.js"></script>
-    <script src="../../assets/js/bootstrap.js"></script>
-    <script src="../../assets/js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="../../assets/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
-    <!-- Custom and plugin javascript -->
-    <script src="../../assets/js/inspinia.js"></script>
-    <script src="../../assets/js/plugins/pace/pace.min.js"></script>
-
-    <!-- Steps -->
-    <script src="../../assets/js/plugins/steps/jquery.steps.min.js"></script>
-
-    <!-- Jquery Validate -->
-	<script src="../../assets/js/plugins/validate/jquery.validate.min.js"></script>
-	
-	<script src="../../assets/js/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
-
+	<?php include '../../includes/parts/user_scripts.php'; ?>
 
     <script>
         $(document).ready(function(){
-            $("#wizard").steps();
-            $("#form").steps({
-                bodyTag: "fieldset",
-                onStepChanging: function (event, currentIndex, newIndex)
-                {
-                    // Always allow going backward even if the current step contains invalid fields!
-                    if (currentIndex > newIndex)
-                    {
-                        return true;
-                    }
 
-                    // Forbid suppressing "Warning" step if the user is to young
-                    if (newIndex === 3 && Number($("#age").val()) < 18)
-                    {
-                        return false;
-                    }
-
-                    var form = $(this);
-
-                    // Clean up if user went backward before
-                    if (currentIndex < newIndex)
-                    {
-                        // To remove error styles
-                        $(".body:eq(" + newIndex + ") label.error", form).remove();
-                        $(".body:eq(" + newIndex + ") .error", form).removeClass("error");
-                    }
-
-                    // Disable validation on fields that are disabled or hidden.
-                    form.validate().settings.ignore = ":disabled,:hidden";
-
-                    // Start validation; Prevent going forward if false
-                    return form.valid();
-                },
-                onStepChanged: function (event, currentIndex, priorIndex)
-                {
-                    // Suppress (skip) "Warning" step if the user is old enough.
-                    if (currentIndex === 2 && Number($("#age").val()) >= 18)
-                    {
-                        $(this).steps("next");
-                    }
-
-                    // Suppress (skip) "Warning" step if the user is old enough and wants to the previous step.
-                    if (currentIndex === 2 && priorIndex === 3)
-                    {
-                        $(this).steps("previous");
-                    }
-                },
-                onFinishing: function (event, currentIndex)
-                {
-                    var form = $(this);
-
-                    // Disable validation on fields that are disabled.
-                    // At this point it's recommended to do an overall check (mean ignoring only disabled fields)
-                    form.validate().settings.ignore = ":disabled";
-
-                    // Start validation; Prevent form submission if false
-                    return form.valid();
-                },
-                onFinished: function (event, currentIndex)
-                {
-                    var form = $(this);
-
-                    // Submit form input
-                    form.submit();
-                }
-            }).validate({
-                        errorPlacement: function (error, element)
-                        {
-                            element.before(error);
-                        },
-                        rules: {
-                            confirm: {
-                                equalTo: "#password"
-                            }
-                        }
-                    });
-
-			
-			document.getElementById('lot').addEventListener('change', function()
+			$('#lot').on('change', function()
 			{
 				var lots = this.value;
+				var obj = [];
 				$('#wf-stp-2').html('');
-				for(let i = 1 ; i <= lots ; i++)
+				for(let i = 0 ; i < lots ; i++)
 				{
+					obj.push({tag: 0});
 					var tmp_lot = `
 					<div class="col-lg-6">
 						<div class="ibox">
 							<div class="ibox-title">
-								<h5>Lot Number ${i}</h5>
+								<h5>Lot Number ${i + 1}</h5>
 							</div>
-							<div class="ibox-content" id="lot.${i}">
+							<div class="ibox-content" id="lot-${i}">
+								<p class="font-bold">Lot Name: </p>
+								<input type="text" class="form-control" name="lot-${i}-title">
 								<div>
-									<p class="font-bold">Header Name: </p>
-									<input type="text" name="lot${i}[list-name][]" class="form-control">
 									<br>
-									<p class="font-bold">Tags:</p>
-									<input class="form-control" name="lot${i}[list][]" data-role="tagsinput">
+									<p class="font-bold">List Name: </p>
+									<input type="text" name="lot-${i}-[list-name][]" class="form-control">
+									<br>
+									<p class="font-bold">&#128204; Tags:</p>
+									<input class="form-control" name="lot-${i}-[list][]" id="lot-${i}-tag-0" data-role="tagsinput">
 									<br>
 								</div>
-							<button type="button" onclick="addList('lot.${i}')">Click</button>
 							</div>
+							<button type="button" data-type="btn" data-tag="lot-${i}">Click</button>
 							
 						</div>
 					</div>`;
-					$('#wf-stp-2').append(tmp_lot);
-					$('[data-role]:last').tagsinput();
+					$(`#wf-stp-2`).append(tmp_lot);
+					$(`#lot-${i}-tag-0`).tagsinput();
 				}
 
-			})
+				$('[data-type="btn"]').on('click', function()
+				{
+					var num = $(this).attr("data-tag").split("-");
+					obj[num[1]].tag++;
+					var tg_num = obj[num[1]].tag;
+					var list_tmp = `
+					<div>
+						<br>
+						<p class="font-bold">List Name: </p>
+						<input type="text" name="lot-${num[1]}-[list-name][]" class="form-control">
+						<br>
+						<p class="font-bold">&#128204; Tags:</p>
+						<input class="form-control" name="lot-${num[1]}-[list][]" id="lot-${num[1]}-tag-${tg_num}" data-role="tagsinput">
+						<br>
+					<div>`;
+					$(`#${num[0]}-${num[1]}`).append(list_tmp);
+					$(`#lot-${num[1]}-tag-${tg_num}`).tagsinput();
+				});
+			});
+		});
 
-	   });
-	   
-
-		function addList(lot)
-		{
-			// var parent = document.getElementById(lot);
-			var num = lot.split(".");
-			var list_tmp = `
-			<div id="lot.${num[1]}">
-				<p class="font-bold">Header Name: </p>
-				<input type="text" name="lot${num[1]}[list-name][]" class="form-control">
-				<br>
-				<p class="font-bold">Tags:</p>
-				<input class="form-control" name="lot${num[1]}[list][]" data-role="tagsinput">
-				<br>
-			</div>`;
-			$('#lot.1').append(list_tmp)
-			$('[data-role]:last').tagsinput();
-		}
     </script>
+
 
 
 </body>
